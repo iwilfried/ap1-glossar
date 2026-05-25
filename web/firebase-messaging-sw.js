148314c0cba77a7fb567a5862ae0c0e6c7cdad77
Wilfried Ifland <wilfried.ifland@gmail.com>
2026-05-25 12:05:20 +0200

    fix(push): upgrade firebase-messaging-sw.js to Firebase JS SDK 12.13.0

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: 'AIzaSyCqXqMZEAu9bPmgxikZqhsLUP8RIQox5-c',
